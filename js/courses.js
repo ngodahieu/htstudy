9e02cac82e26cf7cdf0d0148845a81312bca3548
ngodahieu <ngodahieupbc@gmail.com>
2026-07-07 21:09:51 +0700

    Create courses.js

---
+++ js/courses.js
@@ -0,0 +1,47 @@
+
+/*====================================
+        FILTER COURSE
+====================================*/
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+const courseCards = document.querySelectorAll(".course-card");
+
+filterBtns.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+document.querySelector(".filter-btn.active")
+.classList.remove("active");
+
+button.classList.add("active");
+
+const filter = button.dataset.filter;
+
+courseCards.forEach(card=>{
+
+if(filter==="all"){
+
+card.classList.remove("hide");
+
+return;
+
+}
+
+if(card.dataset.class===filter){
+
+card.classList.remove("hide");
+
+}
+
+else{
+
+card.classList.add("hide");
+
+}
+
+});
+
+});
+
+});
