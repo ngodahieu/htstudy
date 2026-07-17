d135388ac1571b0c3ed6ad2a0d3bfcd43fd54287
ngodahieu <ngodahieupbc@gmail.com>
2026-07-18 02:34:03 +0700

    Update courses.js

--- js/courses.js
+++ js/courses.js
@@ -12,6 +12,145 @@ import {
     getDoc
 }
 from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+/*==========================================
+        NOTIFICATION PANEL
+==========================================*/
+
+const notificationBtn = document.querySelector(".notification-btn");
+
+const notificationPanel =
+document.getElementById("notificationPanel");
+
+const closeNotification =
+document.getElementById("closeNotification");
+
+const notificationBadge =
+document.querySelector(".badge");
+
+notificationBtn.addEventListener("click",(e)=>{
+
+    e.stopPropagation();
+
+    notificationPanel.classList.toggle("active");
+
+    if(notificationPanel.classList.contains("active")){
+
+        notificationBadge.style.opacity="0";
+        notificationBadge.style.transform="scale(0)";
+
+    }
+
+});
+
+closeNotification.addEventListener("click",()=>{
+
+    notificationPanel.classList.remove("active");
+
+});
+
+document.addEventListener("click",(e)=>{
+
+    if(
+
+        !notificationPanel.contains(e.target)
+
+        &&
+
+        !notificationBtn.contains(e.target)
+
+    ){
+
+        notificationPanel.classList.remove("active");
+
+    }
+
+});
+/*==========================================
+        USER MENU
+==========================================*/
+
+const avatar =
+document.querySelector(".avatar");
+
+const userMenu =
+document.getElementById("userMenu");
+
+avatar.addEventListener("click",(e)=>{
+
+    e.stopPropagation();
+
+    userMenu.classList.toggle("active");
+
+});
+
+document.addEventListener("click",(e)=>{
+
+    if(
+
+        !userMenu.contains(e.target)
+
+        &&
+
+        !avatar.contains(e.target)
+
+    ){
+
+        userMenu.classList.remove("active");
+
+    }
+
+});
+const loginOverlay =
+document.getElementById("loginOverlay");
+
+const closeLogin =
+document.getElementById("closeLogin");
+
+const loginBtn =
+document.getElementById("openLogin");
+
+closeLogin.addEventListener("click",()=>{
+
+    loginOverlay.style.display="none";
+
+});
+
+loginBtn.addEventListener("click",()=>{
+
+    userMenu.classList.remove("active");
+
+    loginOverlay.style.display="flex";
+
+});
+
+loginOverlay.addEventListener("click",(e)=>{
+
+    if(e.target===loginOverlay){
+
+        loginOverlay.style.display="none";
+
+    }
+
+});
+const guestBox=document.getElementById("guestBox");
+
+const userBox=document.getElementById("userBox");
+
+const userName=document.getElementById("userName");
+
+const userStudentId=document.getElementById("userStudentId");
+
+const userRole=document.getElementById("userRole");
+
+const userAvatar=document.getElementById("userAvatar");
+
+const logoutBtn=document.getElementById("logoutBtn");
+
+const myCoursesBtn=document.getElementById("myCoursesBtn");
+
+const manageBtn=document.getElementById("manageBtn");
+
+let currentRole="";
 /*====================================
         LẤY DỮ LIỆU TỪ URL
 ====================================*/
